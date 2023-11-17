15e66d0aa95eb471d687a931d89054d06bb8f72f
kirillnev <kirillnev84@gmail.com>
2023-11-17 14:15:31 +0600

    done

--- src/main/java/school/mjc/stage0/conditions/task3/AlphabeticCharacters.java
+++ src/main/java/school/mjc/stage0/conditions/task3/AlphabeticCharacters.java
@@ -2,5 +2,24 @@ package school.mjc.stage0.conditions.task3;
 
 public class AlphabeticCharacters {
     public void vowelDeterminer(char character) {
+        /*
+         Implement the program that will consume a character from method argument
+         which will be from english alphabet and print vowel
+         if it is "Vowel" or consonant if it's "Consonant".
+         And If it is not from eng alphabet than print "wrong alphabet!"
+        */
+        char lowerCaseChar = Character.toLowerCase(character);
+
+        if (lowerCaseChar < 'a' || lowerCaseChar > 'z') {
+            System.out.println("wrong alphabet!");
+        } else if (lowerCaseChar == 'a' ||
+                lowerCaseChar == 'e' ||
+                lowerCaseChar == 'i' ||
+                lowerCaseChar == 'o' ||
+                lowerCaseChar =='u') {
+            System.out.println("Vowel");
+        } else {
+            System.out.println("Consonant");
+        }
     }
 }
--- src/main/java/school/mjc/stage0/conditions/task3/DaysInMonthPrinter.java
+++ src/main/java/school/mjc/stage0/conditions/task3/DaysInMonthPrinter.java
@@ -2,5 +2,19 @@ package school.mjc.stage0.conditions.task3;
 
 public class DaysInMonthPrinter {
     public void amountOfDays(int month) {
+        /*
+         Implement the program that will consume a number month from method argument
+         and print amount of days in that month,
+         if number is not a month number -> print: "wrong number!"
+        */
+        if (month < 1 || month > 12) {
+            System.out.println("wrong number!");
+        } else if (month == 1 || month == 3 || month == 5 || month == 7 || month == 8 || month == 10 || month == 12) {
+            System.out.println(31);
+        } else if (month == 2) {
+            System.out.println(28);
+        } else {
+            System.out.println(30);
+        }
     }
 }
